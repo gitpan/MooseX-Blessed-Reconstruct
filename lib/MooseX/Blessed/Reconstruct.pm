c545fdebb4c95637d02fe4e627925d28603b84ab
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2010-07-13 00:06:18 +0000

    Import of NUFFIN/MooseX-Blessed-Reconstruct-0.04 from CPAN.
    gitpan-cpan-distribution: MooseX-Blessed-Reconstruct
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         NUFFIN/MooseX-Blessed-Reconstruct-0.04.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/MooseX/Blessed/Reconstruct.pm
+++ lib/MooseX/Blessed/Reconstruct.pm
@@ -12,7 +12,7 @@ use Scalar::Util qw(reftype);
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 extends qw(Data::Visitor);
 
@@ -48,8 +48,6 @@ sub visit_object_with_meta {
 	my $args = $v->prepare_args( $meta, $obj );
 
 	$meta->new_object( %$args, __INSTANCE__ => $instance );
-
-	$instance->BUILDALL($args) if $instance->can("BUILDALL");
 
 	return $instance;
 }
@@ -104,7 +102,6 @@ from blessed placeholders
 	# equivalent to:
 
 	my $proper = Foo->meta->new_object(%$obj);
-	$proper->BUILDALL({%$obj});
 
 	# but recursive (and works with shared references)
 
@@ -137,8 +134,6 @@ reconstructed "properly".
 Uses the metaclass C<$meta> to create a new instance, registers the instance
 with L<Data::Visitor>'s cycle tracking, and then inflates it using
 L<Moose::Meta::Class/new_object>.
-
-After the object is built, C<BUILDALL> is called if the object implements it.
 
 =item prepare_args $obj
 
